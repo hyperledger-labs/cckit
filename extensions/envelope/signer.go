99d90041ccfa9b77a90c2b06d1289b5e9d175807
Viktor Nosov <v.nosov@atomyze.team>
2024-06-24 18:33:04 +0300

    crypto/signer
    Signed-off-by: Viktor Nosov <v.nosov@atomyze.team>

--- extensions/envelope/signer.go
+++ extensions/envelope/signer.go
@@ -11,11 +11,11 @@ import (
 )
 
 type Signer struct {
-	crypto Crypto
+	Crypto Crypto
 }
 
 func NewSigner(crypto Crypto) *Signer {
-	return &Signer{crypto: crypto}
+	return &Signer{Crypto: crypto}
 }
 
 func removeSpacesBetweenCommaAndQuotes(s []byte) []byte {
@@ -36,18 +36,18 @@ func (b *Signer) Hash(payload []byte, nonce, channel, chaincode, method, deadlin
 	bb = append(bb, deadline...)
 	b58Pubkey := base58.Encode(pubkey)
 	bb = append(bb, b58Pubkey...)
-	return b.crypto.Hash(bb)
+	return b.Crypto.Hash(bb)
 }
 
 func (b *Signer) Sign(
 	payload []byte, nonce, channel, chaincode, method, deadline string, privateKey []byte) ([]byte, error) {
-	pubKey, err := b.crypto.PublicKey(privateKey)
+	pubKey, err := b.Crypto.PublicKey(privateKey)
 	if err != nil {
 		return nil, fmt.Errorf(`extract public key: %w`, err)
 	}
 
 	hashed := b.Hash(payload, nonce, channel, chaincode, method, deadline, pubKey)
-	return b.crypto.Sign(hashed, privateKey)
+	return b.Crypto.Sign(hashed, privateKey)
 }
 
 func (b *Signer) CheckSignature(payload []byte, nonce, channel, chaincode, method, deadline string, pubKey []byte, sig []byte) error {
